8fb6371be41f1ba5faaa6229ca96ec070149759c
Duchil <duchin.leong@aliyun.com>
2019-07-03 00:03:30 +0800

    路由拦截

--- src/style.js
+++ src/style.js
@@ -9,7 +9,20 @@ export const SpinWrap = styled(Spin)`
   top:50%;
   left:50%;
   transform:translate(-50%,-50%);
-  /* width:60px;
-  height:60px; */
-
+  width:300px;
+  height:300px;
+  /* width: 80%;
+  height: 80%; */
+  .ant-spin{
+    width:300px;
+    height:300px;
+    .ant-spin-dot {
+      width: 100% !important;
+      height: 100% !important;
+      .ant-spin-dot-item{
+        width: 100% !important;
+        height: 100% !important;
+      }
+    }
+  }
 `;
--- src/views/login/style.js
+++ src/views/login/style.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
-import "../../px2rem.scss";
 
 
 export const LoginWrap = styled.div`
-  background: #eff;
+  /* background: #eff; */
 `;
