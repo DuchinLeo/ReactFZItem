5f319a1ca2605d883ca84df004fd130fb2cb339c
Duchil <duchin.leong@aliyun.com>
2019-07-05 01:32:58 +0800

    订单页面数据

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -6,14 +6,25 @@ import { Route, Redirect } from 'react-router-dom';
 class AuthRoute extends Component {
   render () {
     let { component: Component, userinfo, ...rest } = this.props;
-    let user = JSON.parse(window.localStorage.getItem('user'))
+    let user = JSON.parse(window.localStorage.getItem('user'));
     return (
       <Route
         {...rest}
         render={routerProps => {
           // 如果存在登陆的信息，就返回原来的组件,登陆的信息分别存在仓库跟本地了，为了不一刷新就刷掉，所以就 || 本地
-          if (userinfo.username || user.username) {
-            return <Component {...routerProps} />
+          if (user) {
+            if (userinfo.username || user.username) {
+              return <Component {...routerProps} />
+            } else {
+              return (
+                <Redirect
+                  to={{
+                    pathname: '/login',
+                    state: { redirect: routerProps.match.url } // 带跳转的地址过去
+                  }}
+                />
+              )
+            }
           } else {
             return (
               <Redirect
@@ -24,7 +35,8 @@ class AuthRoute extends Component {
               />
             )
           }
-        }}
+        }
+        }
       />
     )
   }
--- src/utils/http.js
+++ src/utils/http.js
@@ -1,21 +1,22 @@
 import axios from 'axios';
-import { message } from 'antd';
+// import { message } from 'antd';
 
 const http = axios.create();
 
-http.defaults.baseURL = '';
+http.defaults.baseURL = 'http://localhost:3001';
 
 http.interceptors.response.use(response => {
   // console.log(response);
   let res = response.data;
-  if (res.code === 0) {
-    // console.log(res);
-    return res;
-  } else {
-    // console.log(res);
-    message.error(res.msg);
+  // if (res.code === 0) {
+  //   // console.log(res);
+  //   return res;
+  // } else {
+  //   // console.log(res);
+  //   message.error(res.msg);
 
-  }
+  // }
+  return res
 });
 
 export default http;
