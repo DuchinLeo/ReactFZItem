0730fa31fbefc0a809e3dc023562aa6e17141871
poiwryyy <1172825513@qq.com>
2019-07-05 08:42:06 +0800

    详细页面底部

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,6 +1,7 @@
 // 拆分出去到组件私有store
 import userLogin from '../views/login/store/reducer';
-// import center from '../views/center/store/reducer';
+import centers from '../views/center/store/reducer';
+
 
 export const userinfo = userLogin;
-// export const center  = center;
+export const center  = centers;
--- src/views/center/store/reducer.js
+++ src/views/center/store/reducer.js
@@ -1,1 +1,41 @@
 // 处理派发动作，然后保存数据的
+import * as Types from './actionTypes'
+
+const initState = {
+  username: {},
+  banner: [],
+  price:{},
+  title:'',
+  sold:{},
+  brand:{},
+  brand1:[],
+  coupon:[],
+  shopCoupons:[],
+  mileage:{},
+  serviceTimeInfo:[],
+
+}
+
+// 把这个仓库的数据暴露出去,在总仓库里面拿到他
+export default (state = initState, action) => {
+  let newState = JSON.parse(JSON.stringify(state))
+
+  // 监听到GET_BANNER类型把传过了的值保存到仓库里面
+  if (action.type === Types.GET_BANNER) {
+    console.log(action.value)
+    newState.username = action.value
+    newState.banner=action.value.banner.data.pics
+    newState.price=action.value.price.data.mainPrice
+    newState.title=action.value.title.data.itemTitle
+    newState.sold=action.value.sold.data
+    newState.brand=action.value.brand.data
+    newState.brand1=action.value.brand.data.cells
+    newState.coupon=action.value.coupon.data.cells
+    newState.shopCoupons=action.value.shopCoupons.data.couponList
+    newState.mileage=action.value.mileage.data
+    newState.serviceTimeInfo=action.value.serviceTimeInfo.data.infos
+  }
+
+// 返回保存的数据
+  return newState;
+}
