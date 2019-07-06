63fea192bffaa806d552f2741c3a79c049132e3e
Duchil <duchin.leong@aliyun.com>
2019-07-06 18:36:46 +0800

    底部抽屉

--- src/utils/MarketChin.js
+++ src/utils/MarketChin.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
 
+
 class MarktChin extends Component {
   render () {
     let { component: Component, id, orderAll, orderObligation, ...rest } = this.props;
@@ -10,26 +11,60 @@ class MarktChin extends Component {
 
     if (orderAll) {
       window.localStorage.setItem('orderAll', JSON.stringify({ orderAll }))
-    } else {
-      var orderAlls = JSON.parse(window.localStorage.getItem('orderAll'))
     }
     if (orderObligation) {
       window.localStorage.setItem('orderObligation', JSON.stringify({ orderObligation }))
-    } else {
-      var orderObligations = JSON.parse(window.localStorage.getItem('orderObligation'))
     }
     return (
       <Route
         {...rest}
         render={routeProps => {
-          if (id === 1) {
+          if (id) {
+            if (id === 1) {
+              if (orderAll) {
+                return (
+                  <Component {...routeProps} data={orderAll} />
+                )
+              } else {
+                var orderAlls = JSON.parse(window.localStorage.getItem('orderAll'))
+                return (
+                  <Component {...routeProps} data={orderAlls.orderAll} />
+                )
+              }
+
+            }
+            if (id === 2) {
+              if (orderObligation) {
+                return (
+                  <Component {...routeProps} data={orderObligation} />
+                )
+              } else {
+                var orderObligations = JSON.parse(window.localStorage.getItem('orderObligation'))
+                return (
+                  <Component {...routeProps} data={orderObligations.orderObligation} />
+                )
+              }
+            }
+          } else {
             return (
-              <Component {...routeProps} data={orderAll} />
-            )
-          }
-          if (id === 2) {
-            return (
-              <Component {...routeProps} data={orderObligation} />
+              <div className="order-null-box">
+                <div className="order-null">
+                  <img src="//gw.alicdn.com/tfs/TB1QXlEQXXXXXcNXFXXXXXXXXXX-320-320.png"
+                    alt="网络异常"
+                    title="没有订单哦"
+                    className="order-null-img"
+                  />
+                  <span className="order-null-text">亲，暂无相关订单
+                  </span>
+                  <div className="order-null-refresh"
+                    onClick={
+                      // 刷新页面
+                      this.locationreload
+                    }>
+                    再试一次
+                  </div>
+                </div>
+              </div>
             )
           }
         }}
